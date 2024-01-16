da554c36546e187f7df9e1d1697310786de11f23
ConquerorCletus <smartchibuike2019@gmail.com>
2024-01-16 20:57:30 +0100

    app_views in index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -3,18 +3,17 @@
 Index module for the API
 """
 from flask import Blueprint, jsonify, abort
+from api.v1.views import app_views
 
-app_views = Blueprint('app_views', __name__)
 
-
-@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized_route() -> str:
     """ Endpoint to test unauthorized error handling
     """
     abort(401, description='Unauthorized')
 
 
-@app_views.route('/api/v1/forbidden', methods=['GET'], strict_slashes=False)
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden_route() -> str:
     """Handle forbidden access"""
     abort(403, description='forbidden')
