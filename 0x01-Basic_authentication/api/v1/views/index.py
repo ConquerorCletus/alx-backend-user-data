2411b3b9c3f2bbeec83aaadec727f91d13e13b2e
ConquerorCletus <smartchibuike2019@gmail.com>
2024-01-16 15:53:29 +0100

    type annotation of index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -7,14 +7,14 @@ from flask import Blueprint, jsonify, abort
 from api.v1.views import app_views
 
 
-@app_views.route('/unauthorized', methods=['GET'])
-def unauthorized_route():
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized_route() -> str:
     """ Endpoint to test unauthorized error handling
     """
     abort(401, description='Unauthorized')
 
 
-@app_views.route('/forbidden', methods=['GET'])
-def forbidden_route():
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden_route() -> str:
     """Handle forbidden access"""
     abort(403, description='forbidden')
