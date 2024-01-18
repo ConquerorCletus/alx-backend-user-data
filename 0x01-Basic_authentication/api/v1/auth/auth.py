2a01797346d7acab847b2d1df88575df9d5c2f4e
ConquerorCletus <smartchibuike2019@gmail.com>
2024-01-18 12:58:41 +0100

    auth.py

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""module contains the auth Class"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """Authentication class."""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Require auth public method."""
+        if path is None:
+            return True
+
+        if excluded_paths is None or excluded_paths == []:
+            return True
+
+        if path in excluded_paths:
+            return True
+
+        path = path.rstrip('/') + '/'
+
+        return path not in excluded_paths
+
+    def authorization_header(self, request=None) -> str:
+        """Authorization header public method"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Current user method"""
+        return None
