e1ec8ed16e55a47edf4590602974b94905976067
ConquerorCletus <smartchibuike2019@gmail.com>
2024-01-19 14:27:26 +0100

    Add a method in api/v1/app.py to handler before_request

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,6 +13,37 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+auth = None  # Initialize auth to None
+AUTH_TYPE = os.getenv("AUTH_TYPE")
+
+if AUTH_TYPE == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def before_request():
+    """
+    Method to handle requests before they reach the route handlers.
+    """
+    if auth is None:
+        pass
+
+    else:
+        excluded_paths = ['/api/v1/status/',
+                          '/api/v1/unauthorized/',
+                          '/api/v1/forbidden/']
+
+        require_auth = auth.require_auth(request.path, excluded_paths)
+
+        if require_auth: 
+
+            if auth.authorization_header(request) is None:
+                abort(401, description='unauthorized')
+
+            if auth.current_user(request) is None:
+                abort(403, description='Forbidden')
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
